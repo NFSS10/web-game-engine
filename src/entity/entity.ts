b2f1b603bed94ec0c9666e0d396ea59de8e5491e
Nuno Silva <22588915+NFSS10@users.noreply.github.com>
2023-12-09 21:45:46 +0000

    [Entity] Improve class

--- src/entity/entity.ts
+++ src/entity/entity.ts
@@ -1,5 +1,23 @@
 class Entity {
-    constructor() {}
+    #id: string;
+    #object: THREE.Object3D;
+
+    constructor(id: string, object: THREE.Object3D) {
+        this.#id = id;
+        this.#object = object;
+    }
+
+    get id(): string {
+        return this.#id;
+    }
+
+    get object(): THREE.Object3D {
+        return this.#object;
+    }
+
+    destroy(): void {
+        throw new Error("Method not implemented.");
+    }
 }
 
 export { Entity };
