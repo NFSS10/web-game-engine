a2d016fb712775f6f78b81096fc02c9dcbbf88bc
Nuno Silva <22588915+NFSS10@users.noreply.github.com>
2023-12-09 21:46:32 +0000

    [Scene] Improve class

--- src/scene/scene.ts
+++ src/scene/scene.ts
@@ -1,8 +1,33 @@
+import * as THREE from "three";
+
+import { Entity } from "@src/entity";
+
 class Scene {
     #id: string;
+    #entities: Entity[] = [];
+    #scene: THREE.Scene;
 
     constructor(id: string) {
         this.#id = id;
+        this.#scene = new THREE.Scene();
+    }
+
+    get id(): string {
+        return this.#id;
+    }
+
+    get scene(): THREE.Scene {
+        return this.#scene;
+    }
+
+    addEntity(entity: Entity): Scene {
+        this.#entities.push(entity);
+        return this;
+    }
+
+    destroy(): void {
+        this.#entities.forEach(entity => entity.destroy());
+        this.#entities = [];
     }
 }
 
