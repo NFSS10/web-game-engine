9a5ba3d9fab7e5ac5185e08945a79acb22bae5c1
Nuno Silva <22588915+NFSS10@users.noreply.github.com>
2024-01-21 17:03:11 +0000

    [Entity Builder] Create primitives base class

---
+++ modules/entity-builder/primitives/base.ts
@@ -0,0 +1,48 @@
+import * as THREE from "three";
+import { type Ammo } from "ammo";
+
+import { Entity } from "@src/entity";
+import { type EntityOptions } from "@src/entity/types";
+import { Physics } from "@src/physics";
+import { type Body, type BodyOptions } from "@src/physics/types";
+
+class PrimitiveEntity extends Entity {
+    #auxTransform: Ammo.btTransform;
+
+    constructor(object: THREE.Object3D, options?: EntityOptions) {
+        super(object, options);
+
+        this.#auxTransform = new Physics.Ammo.btTransform();   
+    }
+
+    createBody(options?: BodyOptions): PrimitiveEntity {
+        throw new Error("Method not implemented");
+    }
+
+    tickBodies(): void {
+        for (let i = 0; i < this.bodies.length; i++) {
+            const body = this.bodies[i] as Body;
+            
+            const ms = body.getMotionState();
+            if (!ms) continue;
+
+            ms.getWorldTransform(this.#auxTransform);
+            const position = this.#auxTransform.getOrigin();
+            const quaternion = this.#auxTransform.getRotation();
+
+            const mesh = this.object;
+            mesh.position.set(position.x(), position.y(), position.z());
+            mesh.quaternion.set(quaternion.x(), quaternion.y(), quaternion.z(), quaternion.w());
+        }
+    }
+
+    destroy(): void {
+        super.destroy();
+
+        Physics.Ammo.destroy(this.#auxTransform);
+        // @ts-expect-error
+        this.#auxTransform = null;
+    }
+}
+
+export { PrimitiveEntity };
