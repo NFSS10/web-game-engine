5c9bbe703441e9fcca0ceee196aab053a28aaea8
Nuno Silva <22588915+NFSS10@users.noreply.github.com>
2024-02-06 22:06:29 +0000

    [Entity Builder] Create `WheelOptions` type

---
+++ modules/entity-builder/raycast-vehicle/types.ts
@@ -0,0 +1,14 @@
+type WheelOptions = {
+    radius?: number;
+    isFrontWheel?: boolean;
+    friction?: number;
+    suspensionStiffness?: number;
+    suspensionDamping?: number;
+    suspensionCompression?: number;
+    suspensionRestLength?: number;
+    rollInfluence?: number;
+    maxEngineForce?: number;
+    maxBrakeForce?: number;
+};
+
+export { type WheelOptions };
