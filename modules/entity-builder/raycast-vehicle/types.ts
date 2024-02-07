d5c55ed8e383445fd7ce8d7e67fd684f6b69e0aa
Nuno Silva <22588915+NFSS10@users.noreply.github.com>
2024-02-07 23:26:22 +0000

    [Entity Builder] Improve types

--- modules/entity-builder/raycast-vehicle/types.ts
+++ modules/entity-builder/raycast-vehicle/types.ts
@@ -1,14 +1,25 @@
-type WheelOptions = {
-    radius?: number;
-    isFrontWheel?: boolean;
-    friction?: number;
-    suspensionStiffness?: number;
-    suspensionDamping?: number;
-    suspensionCompression?: number;
-    suspensionRestLength?: number;
-    rollInfluence?: number;
-    maxEngineForce?: number;
-    maxBrakeForce?: number;
+import { type Ammo } from "ammo";
+
+import { WheelState } from "./enums";
+
+type WheelData = {
+    mesh: THREE.Object3D;
+    state: WheelState;
+    wheelInfo?: Ammo.btWheelInfo;
+    options: WheelOptions;
 };
 
-export { type WheelOptions };
+type WheelOptions = {
+    engineForce: number;
+    brakeForce: number;
+    isFrontWheel: boolean;
+    radius: number;
+    friction: number;
+    suspensionStiffness: number;
+    suspensionDamping: number;
+    suspensionCompression: number;
+    suspensionRestLength: number;
+    rollInfluence: number;
+};
+
+export { type WheelData, type WheelOptions };
