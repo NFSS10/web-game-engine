f72053a0223289d903339ff7e2789bc2e7438ae9
Nuno Silva <22588915+NFSS10@users.noreply.github.com>
2024-02-16 20:23:42 +0000

    [Physics] Support kinematic bodies

--- src/physics/types.ts
+++ src/physics/types.ts
@@ -1,10 +1,13 @@
 import { type Ammo } from "ammo";
 
+import { BodyType } from "./enums";
+
 type Body = Ammo.btRigidBody;
 
 type BodyOptions = {
     mass: number;
     friction: number;
+    type?: BodyType;
 };
 
 export { type Body, type BodyOptions };
