cb31324f341dbfec7bf8e72a92e1550220d5ad7b
Nuno Silva <22588915+NFSS10@users.noreply.github.com>
2024-02-18 00:04:52 +0000

    [Physics] Improve `BodyOptions` type

--- src/physics/types.ts
+++ src/physics/types.ts
@@ -5,9 +5,11 @@ import { BodyType } from "./enums";
 type Body = Ammo.btRigidBody;
 
 type BodyOptions = {
-    mass: number;
-    friction: number;
+    mass?: number;
+    friction?: number;
     type?: BodyType;
+    size?: THREE.Vector3;
+    origin?: THREE.Vector3;
 };
 
 export { type Body, type BodyOptions };
